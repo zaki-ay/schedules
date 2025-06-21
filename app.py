bf5bc7b7a0fcaeafe472b11cd418dd3524844237
Zaki Yahia <zaki.yahia@sectra.com>
2025-06-21 15:25:18 -0400

    fixed path

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import sqlite3
 import getpass
 from functools import lru_cache
+import os
 
 app = Flask(__name__)
 
@@ -9,8 +10,8 @@ BASE_USER = getpass.getuser()
 
 # Database helper functions
 def get_db_connection():
-    conn = sqlite3.connect(f'/home/{BASE_USER}/sched/static/data/database.db')
-#    conn = sqlite3.connect(f'/Users/zico/schedules/static/data/database.db')
+    db_path = os.path.join(os.path.dirname(__file__), 'static', 'data', 'database.db')
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
